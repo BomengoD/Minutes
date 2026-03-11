3faeb5462115e38d294673cbfb2e8facb354a0ad
Ruben Boussa <ruben.boussa@gmail.com>
2026-03-11 15:20:19 +0100

    Sous-programme ConvertMinToHM

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+(int, int) ConvertMinToHM(int nbMin)
+{
+		return (nbMin/60,nbMin%60);
+}
+
+//Programme Principal
+(int H, int M) = ConvertMinToHM(130);
+Console.WriteLine($"{H}:{M}");
