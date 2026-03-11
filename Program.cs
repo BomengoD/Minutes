bb5215a32017102cd549e1991a15529351d2df82
Ruben Boussa <ruben.boussa@gmail.com>
2026-03-11 15:49:50 +0100

    Documentation sous-prog et augmentation README

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Rôle : convertit un nombre de minutes au format H:M
+//un paramètre d'entrée:
+//- nbMin : nombre de minutes à convertir
+//Valeur de retour : un tuple de type (H,M)
+
 (int, int) ConvertMinToHM(int nbMin)
 {
 		return (nbMin/60,nbMin%60);
@@ -9,4 +14,4 @@
 Console.WriteLine("Saisir un nombre de minutes");
 (int H, int M) = ConvertMinToHM(Convert.ToInt32(Console.ReadLine()));
 Console.WriteLine($"{H}:{M:D2}");
-//D
+//D permet de préciser le nombre de chiffres
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Minutes
+
 TD6 de R2.03
+
+## Rôle du logiciel
+
++Convertit un nombre de minutes au format H:M
++Affiche ce résultat H:M
+## Développeur
+Ruben la bête
+
